3139a050e07bf728a1cd8f4f6bfb98f6ac30d8ad
JDIAZ\user <jdiaz@cidenet.com.co>
2023-06-07 14:44:42 -0500

    implementado el tema de generación de token, documentación en un nivel superior al de los proyectos back y front, enrutamiento api y login OK

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+use App\Http\Controllers\JWTController;
+
+Route::group(['prefix' => 'users'], function($router) {
+    Route::post('/register', [JWTController::class, 'register']);
+    Route::post('/login',    [JWTController::class, 'login']);
+    Route::post('/logout',   [JWTController::class, 'logout']);
+    Route::post('/refresh',  [JWTController::class, 'refresh']);
+    // Route::post('/profile', [JWTController::class, 'profile']);
+});
+
+Route::group(['prefix' => 'auth'], function($router) {
+    Route::post('/profile',  [JWTController::class, 'profile']);
+});
